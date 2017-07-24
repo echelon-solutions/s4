c53834f05e861345cc722d78a246685784dfce78
Khalid Zoabi <kzoabi@outlook.com>
2017-07-24 00:53:00 -0400

    Adding the s4-core dependency and the initial deploy command code.

--- src/commands.ts
+++ src/commands.ts
@@ -9,6 +9,9 @@ import * as path from 'path'
 import * as shell from 'shelljs'
 import * as chalk from 'chalk'
 import * as fs from 'fs'
+import * as glob from 'glob'
+import * as core from '@echelon-solutions/s4-core/dist'
+import * as provider from '@echelon-solutions/s4-core/dist/provider/aws'
 
 export interface Options {
   name?: string
@@ -60,7 +63,37 @@ export class Commands {
     })
   }
   deploy (): Promise<void> {
+    let self = this
     return Promise
     .resolve()
+    .then(function (): Promise<void> {
+      let directory = path.resolve('.')
+      let schemaFiles: Array<string> = glob.sync('./schemas/*.json', { cwd: directory })
+      let specificationFiles: Array<string> = glob.sync('./specifications/*.json', { cwd: directory })
+      let resources: Array<core.Resource> = []
+      for (let file of schemaFiles) {
+        console.log('Adding %s as a deployable schema resource.', file)
+        resources.push(new core.Resource(path.basename(file), core.ResourceType.schemas, fs.readFileSync(file, 'utf8'), core.ContentType.JsonSchemaVersion4))
+      }
+      for (let file of specificationFiles) {
+        console.log('Adding %s as a deployable specification resource.', file)
+        resources.push(new core.Resource(path.basename(file), core.ResourceType.specifications, fs.readFileSync(file, 'utf8'), core.ContentType.SwaggerVersion2))
+      }
+      let component = new core.Component('first-time', resources)
+      return provider
+      .initialize(component)
+      .then(function () {
+        return provider.deploy(component)
+      })
+    })
+    .then(function () {
+      console.log(self.color.green('SUCCESS | Project deployed.'))
+      return
+    })
+    .catch(function (error) {
+      console.error(self.color.red(error.message))
+      console.error(self.color.red('ERROR | Unable to deploy the project.'))
+      return
+    })
   }
 }
